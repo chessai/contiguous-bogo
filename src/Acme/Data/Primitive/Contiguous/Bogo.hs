d3fa472a64cba2cb43ad0eebdfaf5c6cd6997afd
chessai <chessai1996@gmail.com>
2019-06-13 11:58:49 -0400

    remove use of shuffleM and instead randomise the array

--- src/Acme/Data/Primitive/Contiguous/Bogo.hs
+++ src/Acme/Data/Primitive/Contiguous/Bogo.hs
@@ -1,3 +1,6 @@
+{-# language BangPatterns #-}
+{-# language ForeignFunctionInterface #-}
+
 -- | Bogosort contiguous datastructures.
 --   To make matters worse, we operate on lists,
 --   and turn those lists back into the original structure.
@@ -5,26 +8,27 @@ module Acme.Data.Primitive.Contiguous.Bogo
   ( bogo
   ) where
 
+import Prelude hiding (read)
+
+import Control.Applicative (liftA2)
+import Control.Monad (void, unless, (>=>))
+import Control.Monad.Primitive (PrimMonad(..))
 import Data.Primitive.Contiguous
 import GHC.Exts (RealWorld)
-import System.Random.Shuffle (shuffleM)
 import System.IO.Unsafe (unsafePerformIO)
-import qualified Data.Foldable as F
 import qualified Data.Primitive.Contiguous as C
 
-data Ordered a = Empty | Decreasing a | Increasing a
-
-inc :: Ordered a -> Bool
-inc (Decreasing _) = False
-inc _              = True
-
-increasing :: (Foldable t, Ord a) => t a -> Bool
-increasing = inc . F.foldl' go Empty where
-  go Empty y = Decreasing y
-  go (Increasing x) _ = Increasing x
-  go (Decreasing x) y
-    | x >= y = Decreasing y
-    | otherwise = Increasing y
+sorted :: (Contiguous arr, Element arr a, Ord a, PrimMonad m)
+  => Mutable arr (PrimState m) a
+  -> m Bool
+sorted !marr = do
+  !sz <- sizeMutable marr
+  let go !ix = if ix < 2
+        then pure True
+        else do
+          b <- liftA2 (<) (read marr (ix - 1)) (read marr (ix - 2))
+          if b then pure False else go (ix - 1)
+  go sz
 
 bogo :: (Contiguous arr, Element arr a, Ord a)
   => arr a
@@ -35,13 +39,49 @@ bogo arr = unsafePerformIO $ do
   C.copy marr 0 arr 0 sz
   bogoMutable marr
   C.unsafeFreeze marr
- 
+
 bogoMutable :: (Contiguous arr, Element arr a, Ord a)
   => Mutable arr RealWorld a
   -> IO ()
 bogoMutable marr = do
-  let go xs = if increasing xs
-        then pure ()
-        else shuffleM xs >>= go
-  marrList <- C.toListMutable marr
-  go marrList
+  let go = do
+        isSorted <- sorted marr
+        unless isSorted $ do
+          randomise marr
+          go
+  go
+
+randomise :: (Contiguous arr, Element arr a)
+  => Mutable arr RealWorld a
+  -> IO ()
+randomise !marr = do
+  srand
+  !sz <- sizeMutable marr
+  let go !ix = if ix > 0
+        then do
+          j <- (`mod` (ix + 1)) <$> c_rand
+          swap marr ix j
+          go (ix - 1)
+        else pure ()
+  go (sz - 1)
+{-# inline randomise #-}
+
+foreign import ccall "srand" c_srand :: Int -> IO Int
+foreign import ccall "time" c_time :: Int -> IO Int
+foreign import ccall "rand" c_rand :: IO Int
+
+srand :: IO ()
+srand = void $ (c_time >=> c_srand) 0
+
+-- | Swap the elements of the mutable array at the given indices.
+swap :: (Contiguous arr, Element arr a, PrimMonad m)
+  => Mutable arr (PrimState m) a
+  -> Int
+  -> Int
+  -> m ()
+swap !marr !ix1 !ix2 = do
+  atIx1 <- read marr ix1
+  atIx2 <- read marr ix2
+  write marr ix1 atIx2
+  write marr ix2 atIx1
+{-# inline swap #-}
